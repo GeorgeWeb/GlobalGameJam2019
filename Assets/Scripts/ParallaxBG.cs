52f6f6fa0d8626bad46beeaea5aa1e99d30f9c52
GeorgeWeb <georgimweb@gmail.com>
2019-01-26 15:56:28 +0000

    Added parallax background

---
+++ Assets/Scripts/ParallaxBG.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBG : MonoBehaviour
+{
+    [SerializeField]
+    float scrollSpeed = 0.1f;
+
+    public MeshRenderer meshRenderer;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        float x = Time.time * scrollSpeed;
+        Vector2 offset = new Vector2(x, 0);
+        meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
