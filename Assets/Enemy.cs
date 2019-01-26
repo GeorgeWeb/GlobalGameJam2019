8c5a4c54cc8dca4923a039cf8275625f555d6643
wolfstrasz <boyan.yotov@abv.bg>
2019-01-26 11:13:55 +0000

    added an enemy spawner

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ground>() != null)
+        {
+         //   gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+          //  gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Debug.Log("Collided with ground");
+        }
+    }
+}
---
+++ Assets/Scripts/Spawning.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+
+    [SerializeField] float minSpawnDelay = 1.0f;
+    [SerializeField] float maxSpawnDelay = 5.0f;
+    [SerializeField] Enemy enemyPrefab;
+    bool spawn = true;
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        transform.SetPositionAndRotation(new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+    private void SpawnAttacker()
+    {
+        Enemy newAttacker = Instantiate(enemyPrefab, transform.position, transform.rotation) as Enemy;
+        newAttacker.transform.parent = transform;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   
+}
