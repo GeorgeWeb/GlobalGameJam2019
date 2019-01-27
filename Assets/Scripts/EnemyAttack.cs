20b96ab929223c896cf820625499dccc08ed24a5
wolfstrasz <boyan.yotov@abv.bg>
2019-01-27 00:44:31 +0000

    few fixes

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
     [SerializeField]
-    int attackDamage = 1;
+    private int attackDamage = 1;
     [SerializeField]
-    float timeBetweenAttacks = 1.5f;
+    private float timeBetweenAttacks = 1.5f;
 
     public GameObject player;
     PlayerHealth playerHealth;
     bool playerInRange; // use 2D collider trigger enter/exit
 
-    float attackTimer;
+    public float attackTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -31,13 +31,14 @@ public class EnemyAttack : MonoBehaviour
         if (attackTimer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
         {
             Attack();
+            // playerInRange = false;
         }
 
         // If the player has zero or less health
         if (playerHealth.currentHealth <= 0)
         {
             // alert player is dead.
-            // Debug.Log("Player is dead!");
+            Debug.Log("Player is dead!");
         }
     }
 
@@ -50,9 +51,13 @@ public class EnemyAttack : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Ended collision with player!");
-        // the player is no longer in range.
-        playerInRange = false;
+        // If the exiting collider is the player
+        if (other.gameObject == player)
+        {
+            Debug.Log("Ended collision with player!");
+            // the player is no longer in range.
+            playerInRange = false;
+        }
     }
 
     void Attack()
